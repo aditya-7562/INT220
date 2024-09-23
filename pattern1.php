ff5406551bad27b42c1fb8176e9427b3d8ff220e
aditya-7562 <aditya.12114424@lpu.in>
2024-09-23 10:37:45 +0530

    initial commit

--- pattern1.php
+++ pattern1.php
@@ -22,15 +22,18 @@ $n = 5;
 
 // pyramid pattern
 
-for ($i = 1; $i <= $n; $i++) {
-    for ($j = $i; $j < $n; $j++) {
-        echo " ";
-    }
-        for ($k = 1; $k <= (2 * $i - 1); $k++) {
-        echo "*";
-    }
-    echo "<br>";
-}
+// for ($i = 1; $i <= $n; $i++) {
+//     for ($j = $i; $j < $n; $j++) {
+//         echo " ";
+//     }
+//         for ($k = 1; $k <= (2 * $i - 1); $k++) {
+//         echo "*";
+//     }
+//     echo "<br>";
+// }
+
+// right angle triangle pattern
+
 
 
 
