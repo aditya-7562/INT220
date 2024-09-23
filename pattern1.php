5247c4e6c07f756d790fa01f3b10806bdf9cc2ba
aditya-7562 <aditya.12114424@lpu.in>
2024-09-23 11:50:45 +0530

    initial commit

--- pattern1.php
+++ pattern1.php
@@ -24,19 +24,31 @@ $n = 5;
 
 // pyramid triangle pattern 
 
-for ($i = 1; $i <= $n; $i++) {
-    for ($j = $i; $j < $n; $j++) {
-        echo " ";
-    }
-        for ($k = 1; $k <= (2 * $i - 1); $k++) {
-        echo "*";
-    }
-    echo "<br>";
-}
+// for ($i = 1; $i <= $n; $i++) {
+//     for ($j = $i; $j < $n; $j++) {
+//         echo " ";
+//     }
+//         for ($k = 1; $k <= (2 * $i - 1); $k++) {
+//         echo "*";
+//     }
+//     echo "<br>";
+// }
 
 // right angle triangle pattern
 
-
+for ($i = 1; $i <= $n; $i++) {
+    // Print spaces
+    for ($j = $i; $j < $n; $j++) {
+        echo " "; // HTML non-breaking space for web output
+    }
+    
+    // Print stars
+    for ($k = 1; $k <= $i; $k++) {
+        echo "*";
+    }
+    
+    echo "<br>"; // HTML line break for web output
+}
 
 
 echo "</pre>";
